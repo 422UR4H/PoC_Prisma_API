e597d557ddcc3ff59995c9a55bd85c4b00f84a7f
422UR4H <tiagosindra@id.uff.br>
2023-09-22 01:19:57 -0300

    :card_file_box: perform/database

--- src/database/db.connection.ts
+++ src/database/db.connection.ts
@@ -1,11 +1,4 @@
-import pg, { ClientConfig } from "pg";
-import dotenv from "dotenv";
+import { PrismaClient } from '@prisma/client';
 
-dotenv.config();
-
-const { Pool } = pg;
-const configDB: ClientConfig = {
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === "production" ? true : false,
-};
-export const clientDB = new Pool(configDB);
+const prisma = new PrismaClient();
+export default prisma;
