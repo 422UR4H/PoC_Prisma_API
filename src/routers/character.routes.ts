15e27c793a9fdcd2dbdabb6c85cae9e7dc5ed1da
422UR4H <tiagosindra@id.uff.br>
2023-09-25 15:43:02 -0300

    :bug: fix: get-by characters routes

--- src/routers/character.routes.ts
+++ src/routers/character.routes.ts
@@ -8,8 +8,8 @@ const router = Router();
 router
     .post("/", validateAuth, validateSchema(characterSchema), create)
     .get("/", readAll)
-    .get("/:id", validateAuth, readById)
-    .get("/:nick", validateAuth, readByNick)
+    .get("/by-id/:id", validateAuth, readById)
+    .get("/by-nick/:nick", validateAuth, readByNick)
     .get("/find-by", find)
     .get("/count", count)
     .put("/:id", validateAuth, validateSchema(characterSchema), update)
